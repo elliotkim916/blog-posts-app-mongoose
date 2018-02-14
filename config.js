d7d179f4b14b5597838480344d7955eb35e9b7e5
Elliot Kim <elliotkim916@gmail.com>
2018-02-13 19:24:15 -0800

    temporarily commented out test url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://elliotkim916:strymontimeline1004@ds133557.mlab.com:33557/blog-posts'
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-posts';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://';
 exports.PORT = process.env.PORT || 8080;
