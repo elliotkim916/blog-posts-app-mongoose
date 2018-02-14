f7e3f0fc566f19763bcf8f2c2accdd88afc3e000
Elliot Kim <elliotkim916@gmail.com>
2018-02-14 14:01:11 -0800

    added comments

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const {BlogPost} = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
+// makes the req.body into json
 
 // GET requests to /blog-posts => returns all blog posts
 app.get('/posts', (req, res) => {
@@ -18,12 +19,11 @@ app.get('/posts', (req, res) => {
         .find()
         .then(blogposts => {
             res.json({ 
-                blogposts: blogposts.map(
+                allBlogPosts: blogposts.map(
                     (post) => post.serialize()) 
             });
         })
-        .catch(
-            err => {
+        .catch(err => {
                 console.error(err);
                 res.status(500).json({message: 'Internal server error'});
     });
@@ -62,7 +62,7 @@ app.post('/posts', (req, res) => {
 
 // PUT request => able to update blog post by ID
 app.put('/posts/:id', (req, res) => {
-    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    if (!(req.params.id === req.body.id)) {
         const message = (`Request path id (${req.params.id}) and request body id (${req.body.id}) must match`);
         console.error(message);
         return res.status(400).json({message: message});
@@ -76,8 +76,8 @@ app.put('/posts/:id', (req, res) => {
     }
 });
 
-// {new: true} allows update to happen after first send
     BlogPost
+    // {new: true} allows update to happen after first send
         .findByIdAndUpdate(req.params.id, {$set: toUpdate}, {new: true})
         .then((post) => res.status(200).json(post.serialize()))
         .catch(err => {
@@ -128,9 +128,9 @@ function closeServer() {
                     return reject(err);
                 }
                 resolve();
-            })
-        })
-    })
+            });
+        });
+    });
 }
 
 if (require.main === module) {
